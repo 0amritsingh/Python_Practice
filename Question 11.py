7edcf36f7b2a3312a90e94b7dbfe5a8e8b35db81
Amrit Singh <72596236+Green-Dev-il@users.noreply.github.com>
2023-08-07 22:11:23 +0530

    Add files via upload

---
+++ Question 11.py
@@ -0,0 +1,15 @@
+# Problem: Write a Python program to test whether a passed letter is a vowel or not.
+
+is_vowel = input("Check you letter is a vowel or not: ")
+if is_vowel == "a":
+    print(f"{is_vowel} is a Vowel")
+elif is_vowel == "e":
+    print(f"{is_vowel} is a Vowel")
+elif is_vowel == "i":
+    print(f"{is_vowel} is a Vowel")
+elif is_vowel == "o":
+    print(f"{is_vowel} is a Vowel")
+elif is_vowel == "u":
+    print(f"{is_vowel} is a Vowel")
+else:
+    print(f"{is_vowel} is NOT a Vowel")
